fd6e506ed2363724da57193336997a485f4e2658
ChowdhuryJoy108 <imjoychy69@gmail.com>
2022-03-20 18:07:21 +0600

    simple profile

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import Profile from './profile/index'
+
+
+
+class App extends Component{
+
+    render(){
+        return (
+        <div className='App'>
+            <Profile />
+        </div>
+        );
+        
+    }
+}
+
+
+
+export default App;
+
+
+
---
+++ src/components/profile/bio.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './bio.style.css'
+
+const Bio = () => (
+    <div className='Bio'>
+                    <h3>Joy Chowdhury</h3>
+                    <p>Fullstack Javascript Developer and Trainer.</p>
+               </div>
+);
+
+export default Bio;
